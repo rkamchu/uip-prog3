b4ff0062a9b377e21c864dcdd0bcb5c6b9f42cee
rkamchu <kam-chu-15@hotmail.com>
2015-10-11 19:13:08 -0500

    Update and rename quiz4.py to quiz3.py

---
+++ Laboratorios/quiz3.py
@@ -0,0 +1,8 @@
+n = 0
+tn =0
+print("Numeros Triangulares")
+num int (input("Ingrese el numero: ""\n"))
+for x in range (num);
+    tri = (x*(x/1))/2
+    if int(tri) <= num;
+        print(str(tri))
--- Laboratorios/quiz4.py
+++
@@ -1,8 +0,0 @@
-chance = 0
-segundos = 0
-while chance < 6:
-    time_seg = int(input("Ingrese el tiempo en segundos: "))
-    chance +=1
-    if time_seg / 60:
-            segundos = 60 - time_seg % 60
-            print(segundos)
